5169be6a83e655f512159d9957e4795b814e96da
jimmyyan05 <jimmy.110013@gmail.com>
2025-04-14 18:17:14 +0800

    first commit

---
+++ todo-list/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TodoList from './components/TodoList.vue'
+</script>
+
+<template>
+  <div class="container max-w-lg mx-auto px-4 pt-12 md:pt-16">
+    <h1 class="text-center text-4xl md:text-5xl font-bold tracking-wider mb-6" 
+        style="font-family: 'Baloo Tamma 2', cursive;">TODO LIST</h1>
+    <TodoList />
+  </div>
+</template>
